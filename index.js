ee6f86e7662ed845052ca8df104c1f68c5a5581b
SusiSusi <suvi.sipila@helsinki.fi>
2018-05-29 15:17:18 +0300

    tehtävät 3.20-3.22

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.use(bodyParser.json())
 app.use(express.static('build'))
 
 app.use(morgan( (tokens, req, res) => {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      JSON.stringify(req.body),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms'
-    ].join(' ')
-  }))
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    JSON.stringify(req.body),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+}))
 
 /*app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
@@ -33,107 +33,107 @@ app.use(morgan( (tokens, req, res) => {
 }*/
 
 app.get('/api/persons', (req, res) => {
-    Person
-        .find({ })
-        .then(persons => {
-            res.json(persons.map(Person.format))
-        })
+  Person
+    .find({ })
+    .then(persons => {
+      res.json(persons.map(Person.format))
+    })
 })
 
 app.get('/info', (req, res) => {
-    const date = new Date()
+  const date = new Date()
 
-    Person
-        .find({ })
-        .then(persons => {
-            res.send(`<p>Puhelinluettelossa ${persons.length} henkilön tiedot.</p><p>${date}</p>`)
-        })
-        .catch(error => {
-            console.log(error)
-        })
+  Person
+    .find({ })
+    .then(persons => {
+      res.send(`<p>Puhelinluettelossa ${persons.length} henkilön tiedot.</p><p>${date}</p>`)
+    })
+    .catch(error => {
+      console.log(error)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    Person
-        .findById(req.params.id)
-        .then(person => {
-            if (person) {
-                res.json(Person.format(person))
-            } else {
-                response.status(404).end()
-            }
-        })  
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
-          })
+  Person
+    .findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(Person.format(person))
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    Person
-        .findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => {
-            res.status(404).send({ error: 'malformatted id' })
-        })
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(result => {
+      result.status(204).end()
+    })
+    .catch(error => {
+      res.status(404).send({ error: 'malformatted id' })
+    })
 })
 
 app.post('/api/persons', (req, res) => {
-    const body = req.body
-    /*const newId = Math.floor(Math.random() * (persons.length - 600) + 600)*/
+  const body = req.body
+  /*const newId = Math.floor(Math.random() * (persons.length - 600) + 600)*/
 
-    if (body.name === "" || body.phone === "") {
-        return res.status(400).json({ error: 'Missing name or number' })
-    }
+  if (body.name === '' || body.phone === '') {
+    return res.status(400).json({ error: 'Missing name or number' })
+  }
 
-    /*if (persons.find(person => person.name === body.name)) {
+  /*if (persons.find(person => person.name === body.name)) {
         return res.status(400).json({ error: 'Name must be unique' })
     }*/
 
-    Person 
-        .find({ name: body.name })
-        .then(result => {
-            if (result.length !== 0) {
-                return res.status(400).json({ error: 'Name must be unique' })
-            } else {
-                const person = new Person ({
-                    name: body.name, 
-                    phone: body.phone
-                })
-
-                person
-                    .save()
-                    .then(Person.format)
-                    .then(savedAndFormattedPerson => {
-                        res.json(savedAndFormattedPerson)
-                     })
-                    .catch(error => {
-                        console.log(error)
-                    })
-            }
-            
+  Person
+    .find({ name: body.name })
+    .then(result => {
+      if (result.length !== 0) {
+        return res.status(400).json({ error: 'Name must be unique' })
+      } else {
+        const person = new Person ({
+          name: body.name,
+          phone: body.phone
         })
+
+        person
+          .save()
+          .then(Person.format)
+          .then(savedAndFormattedPerson => {
+            res.json(savedAndFormattedPerson)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
+
+    })
 })
 
 app.put('/api/persons/:id', (req, res) => {
-    const body = req.body
+  const body = req.body
 
-    const person = {
-        name: body.name,
-        phone: body.phone
-    }
+  const person = {
+    name: body.name,
+    phone: body.phone
+  }
 
-    Person
-        .findByIdAndUpdate(req.params.id, person, { new: true })
-        .then(updatedPerson => {
-            res.json(Person.format(updatedPerson))
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(400).send({ error: 'malformatted id' })
-        })
+  Person
+    .findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(Person.format(updatedPerson))
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const PORT = process.env.PORT || 3001
--- models/person.js
+++ models/person.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 mongoose.Promise = global.Promise
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    phone: String
+  name: String,
+  phone: String
 })
 
 personSchema.statics.format = ({ name, phone, _id }) => {
-    return {
-        name: name,
-        phone: phone,
-        id: _id
-    }
+  return {
+    name: name,
+    phone: phone,
+    id: _id
+  }
 }
 
 const Person = mongoose.model('Person', personSchema)
